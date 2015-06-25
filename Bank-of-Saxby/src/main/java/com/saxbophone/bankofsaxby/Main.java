a0c85693b4dfb885d6d666329e5232817bfec3e7
Joshua Saxby <saxbophone@users.noreply.github.com>
2015-06-25 15:48:56 +0100

    Experiment with sub-package

--- Bank-of-Saxby/src/main/java/com/saxbophone/bankofsaxby/Main.java
+++ Bank-of-Saxby/src/main/java/com/saxbophone/bankofsaxby/Main.java
@@ -1,8 +1,10 @@
 package com.saxbophone.bankofsaxby;
-
+import com.saxbophone.bankofsaxby.other.Tool;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("Bank of Saxby v0.0.0");
+    Tool tl = new Tool();
+    tl.main();
   }
 }
---
+++ Bank-of-Saxby/src/main/java/com/saxbophone/bankofsaxby/other/Tool.java
@@ -0,0 +1,8 @@
+package com.saxbophone.bankofsaxby.other;
+
+
+public class Tool {
+  public static void main() {
+    System.out.println("This is class Tool in sub-package other");
+  }
+}
