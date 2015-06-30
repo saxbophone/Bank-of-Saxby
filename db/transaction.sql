5235719de6c647cbcf2ef7f18c9e99c28a209b67
Joshua Saxby <saxbophone@users.noreply.github.com>
2015-06-30 19:17:57 +0100

    add missing column

--- db/transaction.sql
+++ db/transaction.sql
@@ -6,5 +6,5 @@ CREATE TABLE transaction (
     transfer BOOLEAN, -- type of transaction - TRUE for Transfer involving another account, FALSE for Cash Deposit or Withdrawal
     amount NUMERIC(15, 2) CONSTRAINT non_zero_amount CHECK(@amount > 0.00), -- value of transaction - positive values mean it's a deposit, negative mean it's a withdrawal. The @ operator in the CHECK clause is a Postgres Operator for getting absolute value of numeric.
     CONSTRAINT card_required_for_cash CHECK((card IS NOT NULL AND transfer = FALSE) OR (card IS NOT NULL AND transfer = TRUE) OR (card IS NULL AND transfer = TRUE)), -- constraint only allows non-transfer transactions (cash transactions) to happen if card is present. The OR conjunctions ensure that a card can be present with transfers but is not required.
-    CONSTRAINT cash_transactions_allowed CHECK((initiator.allow_cash AND NOT transfer) OR (transfer = TRUE))
+    CONSTRAINT cash_transactions_allowed CHECK((initiator.allow_cash AND NOT transfer) OR (transfer = TRUE)) -- constraint to check that cash transactions are allowed for this account
 );
