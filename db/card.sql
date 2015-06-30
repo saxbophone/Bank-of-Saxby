02709c1ca7d20e141490deb806426016699fbbaa
Joshua Saxby <saxbophone@users.noreply.github.com>
2015-06-30 18:56:58 +0100

    Updated SQL

--- db/card.sql
+++ db/card.sql
@@ -1,6 +1,7 @@
 CREATE TABLE card (
     id SERIAL PRIMARY KEY, -- Auto-incrementing Primary Key
-    account SERIAL REFERENCES account(id), -- Foreign Key to account table - the account this card belongs to
+    account SERIAL NOT NULL REFERENCES account(id), -- Foreign Key to account table - the account this card belongs to
     no NUMERIC(16), -- 16-digit integer card number
-    pin NUMERIC(4) -- 4-digit card PIN number
+    pin NUMERIC(4) DEFAULT 1234, -- 4-digit card PIN number
+    active BOOLEAN DEFAULT FALSE -- TRUE if this card can authorize transactions / be used, FALSE otherwise
 );
--- db/transaction.sql
+++ db/transaction.sql
@@ -1,7 +1,8 @@
 CREATE TABLE transaction (
     id SERIAL PRIMARY KEY, -- Auto-incrementing Primary Key
-    initiator SERIAL REFERENCES account(id), -- Foreign Key to account table - the account that initiated this transaction
-    participant SERIAL REFERENCES account(id), -- Foreign Key to account table - the other participating account in this transaction (the recipient if withdrawal, the sender if deposit). This field should be null if this is a cash deposit or withdrawal.
+    initiator SERIAL NOT NULL REFERENCES account(id), -- Foreign Key to account table - the account that initiated this transaction
+    participant SERIAL REFERENCES account(id), -- Foreign Key to account table - the other participating account in this transaction (the recipient if withdrawal, the sender if deposit). This field should be null if this is a cash deposit or withdrawal
     card SERIAL REFERENCES card(id) -- Foreign Key to card table - the card that this transaction was authorised under (could be NULL)
-    transfer BOOLEAN -- type of transaction - TRUE for Transfer involving another account, FALSE for Cash Deposit or Withdrawal
+    transfer BOOLEAN, -- type of transaction - TRUE for Transfer involving another account, FALSE for Cash Deposit or Withdrawal
+    amount NUMERIC(15, 2) CHECK(@amount > 0.00) -- value of transaction - positive values mean it's a deposit, negative mean it's a withdrawal. The @ operator in the CHECK clause is a Postgres Operator for getting absolute value of numeric.
 );
