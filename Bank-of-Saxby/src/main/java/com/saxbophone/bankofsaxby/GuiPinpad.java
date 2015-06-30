a8afc91c052f0252038f655ca33c1d0780011949
Joshua Saxby <saxbophone@users.noreply.github.com>
2015-06-30 17:07:27 +0100

    implemented whole Pinpad interface

--- Bank-of-Saxby/src/main/java/com/saxbophone/bankofsaxby/GuiPinpad.java
+++ Bank-of-Saxby/src/main/java/com/saxbophone/bankofsaxby/GuiPinpad.java
@@ -3,19 +3,47 @@ package com.saxbophone.bankofsaxby;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 
 
 public class GuiPinpad extends JPanel implements Pinpad {
+  private boolean capturing = false;
+
   public GuiPinpad() {
     initGui();
   }
+
+  public void startCapture() {
+    capturing = true;
+  }
+
+  public void stopCapture() {
+    capturing = false;
+  }
+
+  public void sendButton(String button) {
+    // Dummy implementation for now
+    // If button is not a number then print on newline:
+    if(Arrays.asList("Cancel", "Clear", "Enter").contains(button)) {
+      System.out.println("");
+      System.out.println(button);
+    }
+    // else then print it out with no newline:
+    else {
+      System.out.print(button);
+    }
+  }
+
   private ActionListener actionListener = new ActionListener() {
     public void actionPerformed(ActionEvent actionEvent) {
-      System.out.println(actionEvent.getActionCommand());
+      if(capturing) {
+        sendButton(actionEvent.getActionCommand());
+      }
     }
   };
+
   public void initGui() {
     setLayout(new GridLayout(5, 3));
     String[] buttons = { "1", "2", "3",
