5b10471b0d7d030c1b02d40b378a734797b68e2b
Joshua Saxby <saxbophone@users.noreply.github.com>
2015-07-07 19:58:32 +0100

    amend transaction, remove illegal constraints

--- db/transaction.sql
+++ db/transaction.sql
@@ -7,7 +7,7 @@ CREATE TABLE transaction (
     amount NUMERIC(15, 2)  NOT NULL,
     CONSTRAINT non_zero_amount CHECK(@amount > 0.00), -- value of transaction - positive values mean it's a deposit, negative mean it's a withdrawal. The @ operator in the CHECK clause is a Postgres Operator for getting absolute value of numeric.
     CONSTRAINT block_negative_transfers CHECK((amount > 0.00 AND cash_transaction = FALSE) OR (cash_transaction = TRUE)), -- Prevent negative transfers to other accounts (this would be withdrawing money from other's accounts otherwise)
-    CONSTRAINT card_required_for_cash CHECK((card IS NOT NULL AND card.active = TRUE AND cash_transaction = TRUE) OR (cash_transaction = FALSE)), -- constraint only allows cash transactions to happen if an active card is present. The OR conjunctions ensure that a card can be present with transfers but is not required.
-    CONSTRAINT cash_transactions_allowed CHECK((initiator.allow_cash AND cash_transaction) OR (cash_transaction = FALSE)), -- constraint that only allows cash transactions if allowed for this account, ignoring this if it's not a cash transaction.
-    CONSTRAINT active_account CHECK(initiator.active = TRUE)
+    CONSTRAINT card_required_for_cash CHECK((card IS NOT NULL AND cash_transaction = TRUE) OR (cash_transaction = FALSE)) -- constraint only allows cash transactions to happen if an active card is present. The OR conjunctions ensure that a card can be present with transfers but is not required.
+    -- CONSTRAINT cash_transactions_allowed CHECK((initiator.allow_cash AND cash_transaction) OR (cash_transaction = FALSE)), -- constraint that only allows cash transactions if allowed for this account, ignoring this if it's not a cash transaction.
+    -- CONSTRAINT active_account CHECK(initiator.active = TRUE)
 );
