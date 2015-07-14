a3c9228d9062c200d2d2a52d902ba0dced35b6f2
Joshua Saxby <saxbophone@users.noreply.github.com>
2015-07-14 17:07:37 +0100

    Add ScreenLayout implementation and demo class

--- Bank-of-Saxby/src/main/java/com/saxbophone/bankofsaxby/ScreenLayout.java
+++ Bank-of-Saxby/src/main/java/com/saxbophone/bankofsaxby/ScreenLayout.java
@@ -2,5 +2,48 @@ package com.saxbophone.bankofsaxby;
 
 
 public class ScreenLayout {
-  // 
+  // This class represents a layout of content on an ATM Screen.
+  // This includes the text next to buttons, main title and message as well as
+  // a reference to the Display that this ScreenLayout should render to.
+  private Display screen;
+  private String title;
+  private String message;
+  private String[] buttonMessages;
+
+  public ScreenLayout(Display target) {
+    // Assign this ScreenLayout to the given Display
+    screen = target;
+  }
+
+  public ScreenLayout(Display target,
+                      String titleText,
+                      String messageText,
+                      String[] buttonMessageText) {
+    // check that there are no more than 6 buttonMessages defined
+    if(buttonMessageText.length > 6) {
+      throw new IllegalArgumentException("Length of array argument " +
+                                         "buttonMessageText cannot exceed 6.");
+    }
+    screen = target;
+    title = titleText;
+    message = messageText;
+    buttonMessages = new String[6];
+    for(int i = 0; i < buttonMessageText.length; i++) {
+      buttonMessages[i] = buttonMessageText[i];
+    }
+  }
+
+  public void render() {
+    // Render the content in this ScreenLayout to it's Display.
+    screen.setHeader(title);
+    screen.setMessage(message);
+    for(int i = 0; i < 6; i++) {
+      if((buttonMessages[i] != null) && (buttonMessages[i] != "")) {
+        screen.setButtonMessage(i, buttonMessages[i]);
+      }
+      else {
+        screen.clearButtonMessage(i);
+      }
+    }
+  }
 }
--- Bank-of-Saxby/src/main/java/com/saxbophone/bankofsaxby/ScreenLayoutDemo.java
+++ Bank-of-Saxby/src/main/java/com/saxbophone/bankofsaxby/ScreenLayoutDemo.java
@@ -1,8 +1,39 @@
 package com.saxbophone.bankofsaxby;
 
+import java.awt.GridLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+
 
 public class ScreenLayoutDemo {
   public static void main(String[] args) {
-    ScreenLayout layout = new ScreenLayout();
+    JFrame screen = new JFrame("ATM Machine Demo");
+    screen.setSize(1024, 480);
+    screen.setLayout(new GridLayout(1, 2));
+    screen.addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent windowEvent){
+        System.exit(0);
+      }
+    });
+    GuiAtmScreen atm = new GuiAtmScreen();
+    ScreenLayout layout = new ScreenLayout(
+        atm,
+        "Ultra-Mega-Corp Ltd ATM Machine",
+        "Please select an option below",
+        new String[] {
+          "Show Balance",
+          "Withdraw Cash",
+          "Print Advice Slip",
+          "Go A.W.O.L.",
+          "See a Doctor",
+          "More Options"
+        });
+    GuiPinpad pad = new GuiPinpad();
+    screen.add(atm);
+    screen.add(pad);
+    screen.setVisible(true);
+    pad.startCapture();
+    layout.render();
   }
 }
